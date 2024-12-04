89f5fc083c55e9f0ee81de80a93953e04bc72600
denisetruuong <denise.truong1124@gmail.com>
2024-12-04 12:43:28 -0600

    quiz cy logic

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -8,3 +8,15 @@ describe("Quiz", ({ test }) => {
     });
 });
 
+cy.mount(<Quiz />);
+
+cy.get("button").should('contain', 'Submit').click();
+for (let i = 0; i < 5; i++) {
+    cy.get("button").click();
+    cy.get("button").should('contain', 'Next').click();
+    cy.get("button").should('contain', 'Submit').click();
+}
+
+cy. get('h2').should('contain', 'You have completed the quiz!');
+cy.get('div[class="score"]').should('contain', 'Your score is 0 out of 5');
+cy.get('button').should('contain', 'Restart Quiz').click();
